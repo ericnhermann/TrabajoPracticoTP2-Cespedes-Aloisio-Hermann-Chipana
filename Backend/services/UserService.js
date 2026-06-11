44aea179d05e1db73921b4d394c35eb266b4cb2d
Cristian Romani Cespedes <42820951@comunidadort.edu.ar>
2026-06-11 17:30:04 -0300

    Correccion, metodos dentro de UserService{}

--- Backend/services/UserService.js
+++ Backend/services/UserService.js
@@ -1,48 +1,50 @@
 class UserService {
-    constructor(user){
+    constructor(user) {
         this.user = user;
     }
-}
 
+    obtenerTodosLosUsuarios = async () => {
+        const usuarios = await this.user.findAll({
+            //esto es para mostrar solo algunos campos de la tabla, en este caso el id, nombre y email
+            attributes: ['id', 'name', 'email'],
+        });
+        return usuarios;
+    };
 
-obtenerTodosLosUsuarios = async () => {
-    const usuarios = await this.user.findAll({
-        //esto es para mostrar solo algunos campos de la tabla, en este caso el id, nombre y email
-        attributes: ['id', 'name', 'email'],
-    });
-    return usuarios;
-};
+    obtenerUsuarioPorId = async (id) => {
+        const usuario = await this.user.findByPk(id, {
+            attributes: ['id', 'name', 'email'],
+        });
+        return usuario;
+    };
 
-obtenerUsuarioPorId = async (id) => {
-    const usuario = await this.user.findByPk(id, {
-        attributes: ['id', 'name', 'email'],
-    });
-    return usuario;
-};
-
-crearUsuario = async (nombre, apellido, email, password) => {
-    const nuevoUsuario = await this.user.create({ name, apellido, email, password });
-    return nuevoUsuario;
-}
-
-actualizarUsuario = async (id, nombre, apellido, email, password) => {
-    const usuario = await this.user.findByPk(id);
-    if (!usuario) {
-        throw new Error('Usuario no encontrado');
+    crearUsuario = async (nombre, apellido, email, password) => {
+        const nuevoUsuario = await this.user.create({ name, apellido, email, password });
+        return nuevoUsuario;
     }
-    //actualizamos los campos del usuario
-    await usuario.update({ name, apellido, email, password });
-    return usuario;
+
+    actualizarUsuario = async (id, nombre, apellido, email, password) => {
+        const usuario = await this.user.findByPk(id);
+        if (!usuario) {
+            throw new Error('Usuario no encontrado');
+        }
+        //actualizamos los campos del usuario
+        await usuario.update({ name, apellido, email, password });
+        return usuario;
+    }
+
+    eliminarUsuario = async (id) => {
+        const usuario = await this.user.findByPk(id);
+        if (!usuario) {
+            throw new Error('Usuario no encontrado');
+        }
+        await usuario.destroy();
+        //podemos retornar el usuario eliminado para confirmar que se elimino correctamente
+        return usuario;
+    }
 }
 
-eliminarUsuario = async (id) => {
-    const usuario = await this.user.findByPk(id);
-    if (!usuario) {
-        throw new Error('Usuario no encontrado');
-    }
-    await usuario.destroy();
-    //podemos retornar el usuario eliminado para confirmar que se elimino correctamente
-    return usuario;
-}
+
+
 
 export default UserService;
